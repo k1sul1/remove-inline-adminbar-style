c2c5fd2bbc32f727dd5ba38ccd0fc6bb6c3f7f05
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-11-24 14:59:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 /*
 Plugin Name: Remove inline adminbar style
 Description: Remove inline style that is added by WordPress to make space for the admin bar. Because !important inline margin sucks.
-Version: 0.1
+Version: 0.1.1
 Author: Christian Nikkanen
 Licence: GPL2
 */
 
-namespace k1sul1;
 
-add_action('get_header', 'remove_inline_style');
-
-function remove_inline_style() {
-  remove_action('wp_head', '_admin_bar_bump_cb');
-}
+add_action('after_setup_theme', function() {
+  add_action('get_header', function() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+  }, 99999999999999);
+});
